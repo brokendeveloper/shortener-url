5267c7cb018e2b5b019423a31417c0860d75db77
brokendev <fluccas09@gmail.com>
2025-07-15 00:44:53 -0300

    refactor(url-access): added the anotations of swagger

--- src/main/java/tech/brokendeveloper/shortener_url/dto/urlAccess/UrlAccessResponseDto.java
+++ src/main/java/tech/brokendeveloper/shortener_url/dto/urlAccess/UrlAccessResponseDto.java
@@ -1,7 +1,11 @@
 package tech.brokendeveloper.shortener_url.dto.urlAccess;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UrlAccessResponseDto(
+        @Schema(description = "Total number of accesses for the shortened URL", example = "42")
         long totalAccesses,
+        @Schema(description = "Average number of accesses per day", example = "3.5")
         double avgPerDay
 ) {
 }
